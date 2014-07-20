acb428bc26c3e2c36d7f5d133be720885f0cc420
Falcon760 <sfalcon266@hotmail.com>
2014-07-20 01:59:59 -0400

    new

---
+++ CardShuffle/CardShuffle/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardShuffle
+{
+   
+
+    static public class FisherYates
+    {
+        static Random r = new Random();
+        //	Based on Java code from wikipedia:
+        //	http://en.wikipedia.org/wiki/Fisher-Yates_shuffle
+        static public void Shuffle(int[] deck)
+        {
+            for (int n = deck.Length - 1; n > 0; --n) //deck of 5 would make n = 4
+            {
+                int a = r.Next(n + 1);  //a is random number out of deck length, ex 5
+                int temp = deck[n];  // temp becomes 4th element
+                deck[n] = deck[a];  //4th element becomes random number
+                deck[a] = temp;  // random element in deck changes places 
+            }
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] somecards = { 1, 2, 3, 4, 5,6,7,8,9,10 };
+            FisherYates.Shuffle(somecards);
+            foreach (var item in somecards)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}

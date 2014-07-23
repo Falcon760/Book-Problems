35c54da4cf9259b76ee55c2d115eb9cbe71f6d2d
Falcon760 <sfalcon266@hotmail.com>
2014-07-22 22:19:07 -0400

    new

---
+++ ConsoleApplication7/ConsoleApplication7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    public class StringHelper
+    {
+        public static string Capitalize(string input)
+        {
+            char[] chararray = input.ToCharArray[];
+            if (char.IsUpper(chararray[0]) == true)
+                char.ToLower(chararray[0]);
+            else
+                char.ToUpper(charrarray[0]);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "john";
+            string result = StringHelper.Captialize(name);
+            //convert the first letter in a string to uppercase
+        }
+    }
+}
---
+++ ConsoleApplication8/ConsoleApplication8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] countries = new string[] { "USA", "France", "Gibaya", "England", "Zibabwe", "South Africa", "Uganda","Japan", "India", "Canada" };
+            string[] states = new string[] {"Michigan", "Minnesota", "Washington", "Texas", "Virginia", "Florida", "Nevada"};
+            //var result = from c in countries
+            //             where c.StartsWith("U")
+            //             orderby c
+            //             select c;
+
+            var result = from c in countries
+                         where c == ""
+
+
+
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);  
+            }
+
+            
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
